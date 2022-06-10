f1252fcffee4bc8e4e71eb72188bf8e0e45ab433
RpRun <remy.paquier@gmail.com>
2022-06-10 16:16:43 +0200

    loader added again

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import './Loader.scss';
+
+const Loader = () => {
+  return (
+    <div class="loader">
+      <div class="spinner">
+        <div class="spinner-bg"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
